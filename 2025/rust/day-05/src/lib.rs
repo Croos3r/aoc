c5e6530f06323121087d0ad486967e11816ae949
Dorian Moy <dorian.moy@proton.me>
2025-12-06 18:32:17 +0100

    feat(part1): implement input parsing and processing logic for part 1
    Took 15 minutes

--- 2025/rust/day-05/src/lib.rs
+++ 2025/rust/day-05/src/lib.rs
@@ -1,2 +1,24 @@
+use itertools::Itertools;
+
 pub mod part1;
 pub mod part2;
+
+fn parse_input(input: &str) -> (Vec<(u64, u64)>, Vec<u64>) {
+    let (fresh_ranges, available_ids) = input.split_once("\n\n").unwrap();
+
+    let fresh_ranges = fresh_ranges
+        .lines()
+        .map(|range| {
+            let (start, end) = range.split_once("-").unwrap();
+
+            (start.parse().unwrap(), end.parse().unwrap())
+        })
+        .collect_vec();
+
+    let available_ids = available_ids
+        .lines()
+        .map(|id| id.parse().unwrap())
+        .collect_vec();
+
+    (fresh_ranges, available_ids)
+}
--- 2025/rust/day-05/src/part1.rs
+++ 2025/rust/day-05/src/part1.rs
@@ -1,17 +1,37 @@
+use crate::parse_input;
+
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day 01 - part 1");
+pub fn process(input: &str) -> miette::Result<usize> {
+    let (fresh_ids, available_ids) = parse_input(input);
+
+    Ok(available_ids
+        .into_iter()
+        .filter(|available_id| {
+            fresh_ids
+                .iter()
+                .any(|(start, end)| start <= available_id && available_id <= end)
+        })
+        .count())
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+	use super::*;
 
-    #[test]
+	#[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32";
+        assert_eq!(3, process(input)?);
         Ok(())
     }
 }
