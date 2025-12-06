37eb3ecf8af6e3bde55da218da738d5646aec0a7
Dorian Moy <dorian.moy@proton.me>
2025-12-06 19:36:03 +0100

    feat(part2): implement processing logic for part 2 and add unit tests
    Took 33 minutes

--- 2025/rust/day-05/src/part2.rs
+++ 2025/rust/day-05/src/part2.rs
@@ -1,17 +1,45 @@
+use crate::parse_input;
+use itertools::Itertools;
+use std::cmp::max;
+
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day 01 - part 2");
+pub fn process(input: &str) -> miette::Result<u64> {
+    let (mut fresh_id_ranges, _) = parse_input(input);
+    fresh_id_ranges.sort();
+
+    let mut result = 0;
+    let mut cur = 0;
+    for (mut start, end) in fresh_id_ranges {
+        if cur >= start {
+            start = cur + 1;
+        }
+        if start <= end {
+            result += end - start + 1;
+        }
+        cur = max(cur, end);
+    }
+
+    Ok(result)
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+	use super::*;
 
-    #[test]
+	#[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32";
+        assert_eq!(14, process(input)?);
         Ok(())
     }
 }
